cbb61e2092cd29eaffcc30077361da6b45e0161a
Endax <client@endax.com>
2011-05-12 23:00:55 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1bbf8db4c531317d921c8d38bd8fef0be0f567284d9172f0fbd0c7fbe3a0c0b24ce1f1b3db56cdf3bbe7ade53914a0f1b65fd07cbcb2bcf2f64c7d2b6b6fead5'
